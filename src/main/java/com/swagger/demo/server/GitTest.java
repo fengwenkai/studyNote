c98574d817deb0c73e6b721cda7189b316b80153
sky7355608 <550454650@qq.com>
2019-03-12 13:35:17 +0800

    ceshi

---
+++ src/main/java/com/swagger/demo/server/GitTest.java
@@ -0,0 +1,12 @@
+package com.swagger.demo.server;
+
+/**
+ * @author WenKai
+ * @description
+ * @create 2019-03-12 13:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello branch");
+    }
+}
