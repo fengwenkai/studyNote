64797b8e81dbd708a8458cdd54ddac1c7e2b40ff
fengwenkai <550454650@qq.com>
2021-05-31 11:19:14 +0800

    1

--- src/main/java/com/swagger/demo/conf/WebServiceConfiguration.java
+++ src/main/java/com/swagger/demo/conf/WebServiceConfiguration.java
@@ -6,4 +6,7 @@ package com.swagger.demo.conf;
  * @create 2019-01-11 10:51
  */
 public class WebServiceConfiguration {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
